9c6a46160c7ce6055705cc014665584e8b7b4792
adrian <158765358+czort23@users.noreply.github.com>
2024-03-29 17:46:51 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// We import the object from the data file. Inside that object there is a function to get players data
+const data = require("./data");
+
+/**
+ * Test 1
+ * Write a function to log in the console the players data with this format:
+ * PLAYER 1
+ * NAME: Zinedine
+ * LASTNAME: Zidane
+ * POSITION: Midfielder
+ * PLAYER 2...
+ */
+
+ // Your code
+
+
+
+/**
+ * Test 2
+ * Write a function to log in the console an array with only the names of the players, ordered by name length descending
+ */
+
+// Your code
+
+
+
+
+
+/**
+ * Test 3
+ * Write a function to log in the console the average number of goals there will be in a match if all the players in the data play on it
+ * scoringChance means how many goals per 100 matches the player will score
+ * Example: 10 players play in a match, each of them has a 0.11 scoringChance, the total number of goals will be 1.1 average 
+ * Output example -> Goals per match: 2.19
+ */
+
+// Your code
+
+
+
+/**
+ * Test 4
+ * Write a function that accepts a name, and logs the position of the player with that name (by position it means striker, goalkeeper...)
+ */
+
+// Your code
+
+
+
+/**
+ * Test 5
+ * Write a function that splits all the players randomly into 2 teams, Team A and Team B. Both teams should have same number of players.
+ * The function should log the match score, using the average number of goals like the Test 3 and rounding to the closest integer
+ * Example:
+ *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance. 
+ *      The average score for the team would be 2.4 (50+50+70+70 / 100), and the closest integer is 2, so the Team A score is 2
+ */
+
+// Your code
